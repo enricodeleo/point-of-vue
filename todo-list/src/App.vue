d87d167de360899ea4212135048a9e631cdd0344
Enrico Deleo <hello@enricodeleo.com>
2018-10-29 00:11:29 +0100

    style: style from mockup

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,6 +1,62 @@
 <template>
   <div id="app-template">
-    <h1>{{ message }}</h1>
+    <header class="container header-main">
+      <div class="row">
+        <div class="column text-center">
+          <h2 class="no-margin"><strong>TODO LIST</strong>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="column">
+          <div class="app-content">
+            <div class="container">
+              <div class="row">
+                <div class="column">
+                  <h2 class="text-white text-medium">
+                    Todo
+                  </h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="column">
+                  <div class="card">
+                    <div class="row">
+                      <div class="column column-20">
+                        <button class="button-check button-clear">
+                          &nbsp;
+                        </button>
+                      </div>
+                      <div class="column column-80">
+                        <h3>Buy Milk</h3>
+                      </div>
+                    </div>
+                    <div class="row task-done">
+                      <div class="column column-20">
+                        <button class="button-check button-clear">
+                          &nbsp;
+                        </button>
+                      </div>
+                      <div class="column column-80">
+                        <h3>Buy Milk</h3>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="column">
+                  <div class="card text-center add-task">
+                    <h3 class="no-margin">Add Task</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,15 +72,95 @@ export default {
 </script>
 
 <style lang="scss">
+  $color-secondary: #9c9c9c;
+
   @import '../node_modules/milligram/src/milligram.sass';
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i');
 
   html,
   body {
+    width: 100%;
+    height: 100%;
+    min-height: 100%;
     padding: 0;
+    margin: 0;
+    background-color: #f8f9f9;
+  }
+
+  body {
+    font-family: 'Open Sans', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Oxygen-Sans", Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    background-image: url('./images/background.jpg');
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .text-left {
+    text-align: left;
+  }
+
+  .text-center {
+    text-align: center;
+  }
+
+  .text-right {
+    text-align: right;
+  }
+
+  .text-medium {
+    font-weight: 500;
+  }
+
+  .text-white {
+    color: #ffffff;
+  }
+
+  .no-margin {
     margin: 0;
   }
 
+  .card {
+    background: #ffffff;
+    padding: 30px 22px;
+    border-radius: 10px;
+  }
+
+  .button-check {
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.20);
+    width: 36px;
+    max-width: 36px;
+    height: 36px;
+    max-height: 36px;
+    padding: 0;
+  }
+
+  .task-done {
+    text-decoration: line-through;
+    color: #cccccc;
+    .button-check {
+      box-shadow: 0 0 10px 0 inset rgba(0, 0, 0, 0.20);
+      background-color: #cccccc;
+    }
+  }
+
   #app-template {
-    background-color: #ed68a1;
+    .header-main {
+      margin-top: 28px;
+      margin-bottom: 28px;
+    }
+
+    .app-content {
+      background-color: #ed68a1;
+      box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.15);
+      max-width: 450px;
+      min-height: 800px;
+      margin: 0 auto;
+      padding: 75px 0;
+    }
+
+    .add-task {
+      padding: 20px;
+      margin-top: 28px;
+    }
   }
 </style>
