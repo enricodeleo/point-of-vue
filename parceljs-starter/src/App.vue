bf32cef85f4049a17de209cd81e85f28ae61fd17
Enrico Deleo <hello@enricodeleo.com>
2018-10-28 20:22:08 +0100

    feature: parceljs starter example

---
+++ parceljs-starter/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app-template">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
