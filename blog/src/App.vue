adfe5cccee467bd6db1c3b8bdfca422c0da5994e
Enrico Deleo <hello@enricodeleo.com>
2019-02-25 23:34:51 +0100

    chore: watch assets

---
+++ blog/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app-template">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+  @import '../node_modules/milligram/src/milligram.sass';
+
+
+  #app-template {
+    background-color: #ed68a1;
+  }
+</style>
